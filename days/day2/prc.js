ec200c2be076b817fe6aa4d4f11afd5e88845895
shaikhmusa6448@gmail.com <shaikhmusa6448@gmail.com>
2024-08-21 13:32:56 +0530

    create code

--- days/day2/prc.js
+++ days/day2/prc.js
@@ -1,10 +1,13 @@
-var os = require('os');
-// console.log(os);
-// console.log(os.cpus());
-// console.log(os.freemem());
-// console.log(os.hostname());
-// console.log(os.getPriority());
-// console.log(os.endianness());
-// console.log(os.homedir());
-// console.log(os.userInfo());
+const os = require("os");
+console.log(os.arch());
+console.log(os.hostname());
+console.log(os.platform());
+console.log(os.tmpdir());
+console.log(os.type());
 
+// const freeMemory=os.freemem()
+// // console.log(freeMemory);
+// console.log(`${freeMemory/1024/1024/1024}`);
+
+// const total=os.totalmem()
+// console.log(`${os.totalmem/1024/1024/1024}`);
