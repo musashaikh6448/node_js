b0326c2800ebb8324926b54afa5c455a88bb5e5a
shaikhmusa6448@gmail.com <shaikhmusa6448@gmail.com>
2024-09-28 18:46:57 +0530

    new folder

---
+++ first project/inde.js
@@ -0,0 +1,33 @@
+let user= {
+    name:"shaikh",
+    mobile:7822896448,
+    email:"shaikh@gmail.com",
+    password: 'shaikh3212'
+}
+
+
+// let n1= user.name
+// let m1= user.mobile
+// let e1= user.email
+// let p1= user.password
+
+
+
+// let {name:n1,mobile:m1,email:e1,password:p1}=user
+// console.log(n1);
+// console.log(m1);
+// console.log(e1);
+// console.log(p1);
+
+
+// let name= name
+// let mobile= mobile
+// let email= email
+// let password= password
+
+let {name,mobile,email,password}=user
+
+console.log(name);
+console.log(mobile);
+console.log(email);
+console.log(password);
